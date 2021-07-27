d9d6c6211ee6211b3114ad230c7a4cedde532542
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-07-27 17:39:13 +0530

    Commit
    Commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import Welcome from './Welcome';
-
+import MyForm from './MyFrom';
 // create other components and import them here. 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Welcome name="Vaman" phone="9876"> </Welcome>
+      <Welcome name="Vaman" phone="9876543210"> </Welcome>
       <h1>App Component</h1>
       <p>This is the app component.</p>
       <Footer/>
+      <MyForm/>
     </div>
   );
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 // Function component 
 
+let fun = () => {return 'fun...'}
+
+
 let Welcome = (props) => {
 
     return (
         <div>
-            <h1>Hello, {props.name} </h1>
+            <h1>Hello, { props.name } </h1>
             <p> {props.phone} </p>
+            <p> {fun()} </p>
         </div>
     );
 }
@@ -16,6 +20,9 @@ export default Welcome;
 
 // Class component 
 // class Welcome extends React.Component {
+    // construtor(props) {
+
+    // }
 
 //     render() {
 //     return <h1>Hello, {}</h1>;
