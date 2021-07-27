3b1f588959f3b35ba5629af138dc61b960266094
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-07-27 18:09:17 +0530

    Commit
    Commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Footer from './Footer';
 import Welcome from './Welcome';
 import MyForm from './MyFrom';
+import Employee from './Employee';
 // create other components and import them here. 
 
 function App() {
@@ -13,8 +14,10 @@ function App() {
       <Welcome name="Vaman" phone="9876543210"> </Welcome>
       <h1>App Component</h1>
       <p>This is the app component.</p>
+
       <Footer/>
       <MyForm/>
+      <Employee/>
     </div>
   );
 }
