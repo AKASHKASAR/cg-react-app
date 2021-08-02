3cfdcb3d55da95aacf4c3ecd5db114fa2be59029
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-08-02 17:25:05 +0530

    Update JavaData.js

--- src/components/training/JavaData.js
+++ src/components/training/JavaData.js
@@ -6,9 +6,7 @@ let JavaData = (props) => {
     const [parentEmp, setParentEmp] = useState({}); // from props  
     const [parentEmpHike, setParentEmpHke] = useState(0); // from props 
     const [childEmp, setChildEmp] = useState({}); // child state   
-
     const [emp, setEmp] = useState({}); // from axios 
-
 
     // initilize states to prevent undefined error, in case used these fields anywhere else in future 
     useEffect(() => {
@@ -33,23 +31,23 @@ let JavaData = (props) => {
         props.parentCallback(childEmp);
     };
 
-    // invoke post method with handlesubmit to add an employee   
-    const submitAddEmployee = (event) => {
-        axios.post(`/addEmployee`, emp)
-            .then((response) => {
-                console.log(response.data.name);
-            }).catch((error) => {
-                console.log(error.message)
-            });
-        event.preventDefault();
-    }
-
     const handleEmployee = (event) => {
+        console.log(emp.id);
         setEmp({
             ...emp,
             [event.target.name]: event.target.value
         });
     };
+
+    const getAllEmployees = (e) => {
+        console.log();
+        axios.get(`/getAllEmployees`)
+            .then((response) => {
+                setEmpList(response.data);
+            }).catch(error => {
+                console.log(error.message)
+            });
+    }
 
     const submitGtEmployeById = (event) => {
         axios.get(`/getEmployee/${emp.id}`)
@@ -65,40 +63,45 @@ let JavaData = (props) => {
         event.preventDefault();
     }
 
-    const submitUpdateEmployee = (event) => {
-        axios.get(`/getEmployee/${emp.id}`)
-            .then(
-                (response) => {
-                    setEmp(response.data);
-                    console.log(emp.id);
-                }
-            )
-            .catch((error) => {
-                console.log(error.message);
+    const submitAddEmployee = (event) => {
+        axios.post(`/addEmployee`, emp)
+            .then((response) => {
+                console.log(response.data.name);
+            }).catch((error) => {
+                console.log(error.message)
             });
         event.preventDefault();
     }
 
-    const getAllEmployees = (e) => {
-        console.log();
-        axios.get(`/getAllEmployees`)
+    const submitUpdateEmployee = (event) => {
+        axios.put(`/updateEmployee/`, emp)
             .then((response) => {
-                setEmpList(response.data);
-            }).catch(error => {
+                console.log(response.data.name);
+            }).catch((error) => {
                 console.log(error.message)
             });
+        event.preventDefault();
+    }
+
+    const submitDeleteEmployee = (event) => {
+        axios.delete(`/deleteEmployee/${emp.id}`)
+            .then((response) => {
+                console.log(response.data.name);
+            }).catch((error) => {
+                console.log(error.message)
+            });
+        event.preventDefault();
     }
 
     return (
         <div>
             <p>JavaData Component</p>
-            <p>axios {empList.map(e => <div>{e.name}</div>)}</p>
             <p>axios {emp.name}</p>
             <p>child {parentEmp.name}</p>
             <p> child {parentEmpHike} </p>
             <button onClick={onTrigger}>Pass child emp to parent</button>
-            <p>Add new Employee</p>
             <div>
+                <p>Add new Employee</p>
                 <form onSubmit={submitAddEmployee}>
                     <div>
                         <input
@@ -126,13 +129,63 @@ let JavaData = (props) => {
                     </div>
                     <button type="submit" className="btn btn-primary">Add Employee</button>
                 </form>
+                <p> New employee added {emp.id} , {emp.name} , {emp.salary} </p>
+            </div>
+
+            <div>
+                <p>Update an Employee</p>
+                <form onSubmit={submitUpdateEmployee}>
+                    <div>
+                        <input
+                            type="number"
+                            id="id"
+                            name="id"
+                            value={emp.id}
+                            placeholder="143" // add for other elements as well 
+                            onChange={handleEmployee}
+                        />
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={emp.name}
+                            onChange={handleEmployee}
+                        />
+                        <input
+                            type="number"
+                            id="salary"
+                            name="salary"
+                            value={emp.salary}
+                            onChange={handleEmployee}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Update Employee</button>
+                </form>
                 <div>
-                    <p> New employee added {emp.id} , {emp.name} , {emp.salary} </p>
+                    <p> Employee Updated - {emp.id} , {emp.name} , {emp.salary} </p>
                 </div>
             </div>
+
             <div>
-                <p>fun</p>
+                <p>Delete an Employee</p>
+                <form onSubmit={submitDeleteEmployee}>
+                    <div>
+                        <input
+                            type="number"
+                            id="id"
+                            name="id"
+                            value={emp.id}
+                            placeholder="143" // add for other elements as well 
+                            onChange={handleEmployee}
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Delete Employee</button>
+                </form>
+                <p>Employee deleted {emp.id}</p>
+
             </div>
+
+
             <div>
                 <form onSubmit={submitGtEmployeById}>
                     <div>
@@ -151,6 +204,7 @@ let JavaData = (props) => {
                     <p>Employee Data - {emp.id} , {emp.name} , {emp.salary} </p>
                 </div>
                 <div>
+                    <p>Get all employees data </p>
                     <button type="submit" className="btn btn-primary" onClick={getAllEmployees}>Get All Employees</button>
                     <p>All Employee Data</p>
                     <div> {empList.map(e => <p> {e.id}, {e.name} {e.salary} </p>)} </div>
