6844f6ccb04072919b6f5c7c1e8feb62a89e7d17
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-08-02 11:48:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,16 @@ import store from './redux/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
-)
+);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 // https://create-react-app.dev/docs/measuring-performance/
 reportWebVitals();
