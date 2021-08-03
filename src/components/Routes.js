f6e4c438f5302ad515b8c3e2bc5d9f04c5af3d51
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-08-03 11:22:03 +0530

    Update Routes.js

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -37,7 +37,7 @@ const Routes = () => {
                     </div>
                 </div>
             </Router>
-            <Footer />
+            {/* <Footer /> */}
         </div>
     );
 };
