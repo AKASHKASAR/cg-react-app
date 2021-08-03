37b10317afd106c9b9c93dc2d468064918250d76
Vaman Deshmukh <vamandeshmukh@gmail.com>
2021-08-03 15:05:09 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Calc from './components/training/Calc';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+describe(`Test Employee Component`, () => {
+
+  it(`test Calc equals 10`, () => {
+    let num = Calc();
+    expect(num).toBe(10);
+  });
+
+  it(`test Calc not equals 5`, () => {
+    let num = Calc();
+    expect(num).not.toBe(5);
+  });
+
 });
